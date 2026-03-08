c1b4dc906d42b2d19509ae844f81efc75769bb92
Fatima <fatim.torre@gmail.com>
2026-03-07 23:13:09 -0600

    cleanUp

--- src/router.js
+++ src/router.js
@@ -7,12 +7,7 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomePage,
-  },
-  {
-    path: '/other',
-    name: 'other',
-    component: () => import('@/views/OtherPage.vue'),
-  },
+  }
 ]
 
 const router = createRouter({
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,17 @@
 <script setup>
-import { ref } from 'vue'
-const title = ref('Hello There ✋')
+
 </script>
 
 <template>
-  <main class="flex flex-1 items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <main class="min-h-screen bg-[radial-gradient(circle_at_15%_20%,#334155,transparent_40%), radial-gradient(circle_at_%85_%0,#0f766e,transparent_28%), linear-gradient(160deg,#020617,#0f172a,#111827)] px-4 py-8 text-slate-100 sm:px-6 lg:px-8"></main>
+
+  <div class="mx-auto max-w-7xl">
+    <header>
+      <p> Project #4</p>
+      <h1> Anime Roulette Machine</h1>
+      <p>Spin the Wheel, request a random anime from Jinkan with VueUse useFetch, and learn how REST APIs signal rate limiting with HTTP 429
+        
+      </p>
+    </header>
+  </div>
 </template>
